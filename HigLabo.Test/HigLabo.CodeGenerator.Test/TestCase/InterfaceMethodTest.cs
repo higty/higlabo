b06bda868d6ad438bf50ec084d51f4e55ff20f6d
higty <higuchihiguchihiguchi@hotmail.com>
2015-02-24 11:50:11 +0900

    Add DbSharp/Reference folder. Add HigLabo.CodeGenerator.Test project.

---
+++ HigLabo.Test/HigLabo.CodeGenerator.Test/TestCase/InterfaceMethodTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HigLabo.CodeGenerator;
+using System.IO;
+
+namespace HigLaboTest
+{
+    [TestClass]
+    public class InterfaceMethodTest
+    {
+        [TestMethod]
+        public void InterfaceMethodBasicFeature()
+        {
+            var md = new InterfaceMethod("GetNumber");
+
+            Assert.AreEqual("void GetNumber();" + SourceCodeGenerator.NewLine
+                , SourceCodeGenerator.Write(SourceCodeLanguage.CSharp, md));
+        }
+        [TestMethod]
+        public void InterfaceMethodParameters()
+        {
+            var md = new InterfaceMethod("GetDisplayName");
+            md.Parameters.Add(new MethodParameter("String", "name"));
+            md.Parameters.Add(new MethodParameter("Int32", "age"));
+
+            Assert.AreEqual("void GetDisplayName(String name, Int32 age);" + SourceCodeGenerator.NewLine
+                , SourceCodeGenerator.Write(SourceCodeLanguage.CSharp, md));
+        }
+        [TestMethod]
+        public void InterfaceMethodGenericParameters()
+        {
+            var md = new InterfaceMethod("GetDisplayName");
+            md.AddParameter("String", "name");
+            md.AddParameter("Int32", "age");
+            md.GenericParameters.Add("T1");
+            md.GenericParameters.Add("T2");
+
+            Assert.AreEqual("void GetDisplayName<T1, T2>(String name, Int32 age);" + SourceCodeGenerator.NewLine
+                , SourceCodeGenerator.Write(SourceCodeLanguage.CSharp, md));
+        }
+        [TestMethod]
+        public void InterfaceMethodPartialFeature()
+        {
+            var md = new InterfaceMethod("GetNumber");
+            //These will be ignored
+            md.Parameters.Add(new MethodParameter("Int32", "number"));
+            md.GenericParameters.Add("T");
+            md.ReturnTypeName = new TypeName("Int32");
+
+            Assert.AreEqual("Int32 GetNumber<T>(Int32 number);" + SourceCodeGenerator.NewLine
+                , SourceCodeGenerator.Write(SourceCodeLanguage.CSharp, md));
+        }
+    }
+}
---
+++ Tools/DbSharp/Reference/CopyFromHigLabo.cmd
@@ -0,0 +1,42 @@
+SET path="C:\Users\F03006\Documents\Visual Studio 2013\Projects\HigLabo\
+SET targetPath="C:\Users\F03006\Documents\Visual Studio 2013\Projects\HigLabo\Tools\DbSharp\Reference\
+
+Copy %path%Reference\Net4.5\Newtonsoft.Json.dll" %targetPath%Newtonsoft.Json.dll
+
+Copy %path%Reference\Net4.5\MySql.Data.dll" %targetPath%MySql.Data.dll"
+Copy %path%Reference\Net4.5_32bit\Oracle.DataAccess.dll" %targetPath%Oracle.DataAccess.dll"
+Copy %path%Reference\Net4.5\Npgsql\NpgSql.dll" %targetPath%NpgSql.dll"
+Copy %path%Reference\Net4.5\System.Data.SQLite.dll" %targetPath%System.Data.SQLite.dll"
+
+Copy %path%Reference\Net4.5\MySql.Data.dll" %targetPath%MySql.Data.dll"
+Copy %path%Reference\Net4.5_32bit\Oracle.DataAccess.dll" %targetPath%Oracle.DataAccess.dll"
+Copy %path%Reference\Net4.5\Npgsql\NpgSql.dll" %targetPath%NpgSql.dll"
+Copy %path%Reference\Net4.5\System.Data.SQLite.dll" %targetPath%System.Data.SQLite.dll"
+
+Copy %path%HigLabo.Core\bin\Release\_Net_4_5\HigLabo.Core.dll" %targetPath%HigLabo.Core.dll"
+Copy %path%HigLabo.Mail\bin\Release\_Net_4_5\HigLabo.Converter.dll" %targetPath%HigLabo.Converter.dll"
+Copy %path%HigLabo.Mapper\bin\Release\_Net_4_5\HigLabo.Mapper.dll" %targetPath%HigLabo.Mapper.dll"
+
+Copy %path%HigLabo.CodeGenerator\bin\Release\_Net_4_5\HigLabo.CodeGenerator.dll" %targetPath%HigLabo.CodeGenerator.dll"
+
+Copy %path%HigLabo.Data\bin\Release\_Net_4_5\HigLabo.Data.dll" %targetPath%HigLabo.Data.dll"
+Copy %path%HigLabo.Data.Oracle\bin\Release\_Net_4_5\HigLabo.Data.Oracle.dll" %targetPath%HigLabo.Data.Oracle.dll"
+Copy %path%HigLabo.Data.MySql\bin\Release\_Net_4_5\HigLabo.Data.MySql.dll" %targetPath%HigLabo.Data.MySql.dll"
+Copy %path%HigLabo.Data.PostgreSql\bin\Release\_Net_4_5\HigLabo.Data.PostgreSql.dll" %targetPath%HigLabo.Data.PostgreSql.dll"
+Copy %path%HigLabo.Data.SqLite\bin\Release\_Net_4_5\HigLabo.Data.SqLite.dll" %targetPath%HigLabo.Data.SqLite.dll"
+
+Copy %path%HigLabo.Rss\bin\Release\_Net_4_5\HigLabo.Rss.dll" %targetPath%HigLabo.Rss.dll"
+Copy %path%HigLabo.Net\bin\Release\_Net_4_5\HigLabo.Net.dll" %targetPath%HigLabo.Net.dll"
+Copy %path%HigLabo.Net.Ftp\bin\Release\_Net_4_5\HigLabo.Net.Ftp.dll" %targetPath%HigLabo.Net.Ftp.dll"
+
+Copy %path%HigLabo.Mail\bin\Release\_Net_4_5\HigLabo.Mime.dll" %targetPath%HigLabo.Mime.dll"
+Copy %path%HigLabo.Mail\bin\Release\_Net_4_5\HigLabo.Mail.dll" %targetPath%HigLabo.Mail.dll"
+Copy %path%HigLabo.Net.Twitter\bin\Release\_Net_4_5\HigLabo.Net.Twitter.dll" %targetPath%HigLabo.Net.Twitter.dll"
+
+Copy %path%HigLabo.Web\bin\Release\_Net_4_5\HigLabo.Web.dll" %targetPath%HigLabo.Web.dll"
+Copy %path%HigLabo.Wpf\bin\Release\_Net_4_5\HigLabo.Wpf.dll" %targetPath%HigLabo.Wpf.dll"
+
+
+
+
+pause
