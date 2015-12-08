e0eafcfac5e8eddd38538500bb905c915fa04ff3
higty <higuchihiguchihiguchi@hotmail.com>
2015-12-08 12:04:39 +0900

    Modify bug of EnumExtensions.ToStringFromEnum when call this method to HttpStatusCode Enum.

--- HigLabo.Test/HigLabo.Core.Test/EnumExtensionsTest.cs
+++ HigLabo.Test/HigLabo.Core.Test/EnumExtensionsTest.cs
@@ -40,38 +40,110 @@ namespace HigLabo.Core.Test
         [TestMethod]
         public void BasicEnumTest()
         {
-            Assert.AreEqual(DayOfWeek.Friday.ToStringFromEnum(), "Friday");
-            Assert.AreEqual(DayOfWeek.Wednesday.ToStringFromEnum(), "Wednesday");
+            Assert.AreEqual("Friday", DayOfWeek.Friday.ToStringFromEnum());
+            Assert.AreEqual("Wednesday", DayOfWeek.Wednesday.ToStringFromEnum());
+            Assert.AreEqual("Saturday", DayOfWeek.Saturday.ToStringFromEnum());
+            Assert.AreEqual("Friday", ((DayOfWeek)(5)).ToStringFromEnum());
+            Assert.AreEqual("Saturday", ((DayOfWeek)(6)).ToStringFromEnum());
         }
         [TestMethod]
         public void BaseIsNotZeroEnumTest()
         {
-            Assert.AreEqual(BaseIsNotZeroEnum.V2.ToStringFromEnum(), "V2");
-            Assert.AreEqual(BaseIsNotZeroEnum.V3.ToStringFromEnum(), "V3");
-            Assert.AreEqual(BaseIsNotZeroEnum.V4.ToStringFromEnum(), "V4");
+            Assert.AreEqual("V2", BaseIsNotZeroEnum.V2.ToStringFromEnum());
+            Assert.AreEqual("V3", BaseIsNotZeroEnum.V3.ToStringFromEnum());
+            Assert.AreEqual("V4", BaseIsNotZeroEnum.V4.ToStringFromEnum());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyValueTest()
+        {
+            String s = "";
+            s = ((DayOfWeek)(-1)).ToStringFromEnum();
+            s = ((DayOfWeek)(7)).ToStringFromEnum();
+            s = ((HttpStatusCode)(3)).ToStringFromEnum();
         }
         [TestMethod]
         public void ByteEnumTest()
         {
-            Assert.AreEqual(ByteEnum.V0.ToStringFromEnum(), "V0");
-            Assert.AreEqual(ByteEnum.V2.ToStringFromEnum(), "V2");
-            Assert.AreEqual(ByteEnum.V4.ToStringFromEnum(), "V4");
+            Assert.AreEqual("V0", ByteEnum.V0.ToStringFromEnum());
+            Assert.AreEqual("V2", ByteEnum.V2.ToStringFromEnum());
+            Assert.AreEqual("V4", ByteEnum.V4.ToStringFromEnum());
         }
         [TestMethod]
         public void LongEnumTest()
         {
-            Assert.AreEqual(LongEnum.V0.ToStringFromEnum(), "V0");
-            Assert.AreEqual(LongEnum.V2.ToStringFromEnum(), "V2");
-            Assert.AreEqual(LongEnum.V1620100.ToStringFromEnum(), "V1620100");
-            Assert.AreEqual(LongEnum.V23372036854775807.ToStringFromEnum(), "V23372036854775807");
+            Assert.AreEqual("V0", LongEnum.V0.ToStringFromEnum());
+            Assert.AreEqual("V2", LongEnum.V2.ToStringFromEnum());
+            Assert.AreEqual("V1620100", LongEnum.V1620100.ToStringFromEnum());
+            Assert.AreEqual("V23372036854775807", LongEnum.V23372036854775807.ToStringFromEnum());
         }
         [TestMethod]
         public void FlagEnumTest()
         {
-            Assert.AreEqual(FlagsEnum.One.ToStringFromEnum(), "One");
-            Assert.AreEqual((FlagsEnum.One | FlagsEnum.Two).ToStringFromEnum(), "One,Two");
-            Assert.AreEqual(FlagsEnum.Three.ToStringFromEnum(), "Three");
-            Assert.AreEqual(FlagsEnum.Four.ToStringFromEnum(), "Four");
+            Assert.AreEqual("One", FlagsEnum.One.ToStringFromEnum());
+            Assert.AreEqual("One,Two", (FlagsEnum.One | FlagsEnum.Two).ToStringFromEnum());
+            Assert.AreEqual("Three", FlagsEnum.Three.ToStringFromEnum());
+            Assert.AreEqual("Four", FlagsEnum.Four.ToStringFromEnum());
         }
+        [TestMethod]
+        public void HttpStatusCodeEnumTest()
+        {
+            Assert.AreEqual("Continue", HttpStatusCode.Continue.ToStringFromEnum());
+            Assert.AreEqual("BadRequest", HttpStatusCode.BadRequest.ToStringFromEnum());
+            Assert.AreEqual("Forbidden", HttpStatusCode.Forbidden.ToStringFromEnum());
+            Assert.AreEqual("InternalServerError", HttpStatusCode.InternalServerError.ToStringFromEnum());
+            Assert.AreEqual("OK", ((HttpStatusCode)(200)).ToStringFromEnum());
+        }
+    }
+
+    public enum HttpStatusCode
+    {
+        Continue = 100,
+        SwitchingProtocols = 101,
+        OK = 200,
+        Created = 201,
+        Accepted = 202,
+        NonAuthoritativeInformation = 203,
+        NoContent = 204,
+        ResetContent = 205,
+        PartialContent = 206,
+        MultipleChoices = 300,
+        Ambiguous = 300,
+        MovedPermanently = 301,
+        Moved = 301,
+        Found = 302,
+        Redirect = 302,
+        SeeOther = 303,
+        RedirectMethod = 303,
+        NotModified = 304,
+        UseProxy = 305,
+        Unused = 306,
+        TemporaryRedirect = 307,
+        RedirectKeepVerb = 307,
+        BadRequest = 400,
+        Unauthorized = 401,
+        PaymentRequired = 402,
+        Forbidden = 403,
+        NotFound = 404,
+        MethodNotAllowed = 405,
+        NotAcceptable = 406,
+        ProxyAuthenticationRequired = 407,
+        RequestTimeout = 408,
+        Conflict = 409,
+        Gone = 410,
+        LengthRequired = 411,
+        PreconditionFailed = 412,
+        RequestEntityTooLarge = 413,
+        RequestUriTooLong = 414,
+        UnsupportedMediaType = 415,
+        RequestedRangeNotSatisfiable = 416,
+        ExpectationFailed = 417,
+        UpgradeRequired = 426,
+        InternalServerError = 500,
+        NotImplemented = 501,
+        BadGateway = 502,
+        ServiceUnavailable = 503,
+        GatewayTimeout = 504,
+        HttpVersionNotSupported = 505
     }
 }
