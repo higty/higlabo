3a60c08c3a41a3438e2b41def802daa5ef09e063
higty <higuchihiguchihiguchi@hotmail.com>
2016-01-06 12:37:53 +0900

    Add ParameterDataProviderFromHttpRouteData.cs file.

---
+++ HigLabo.Web/Mvc/ParameterDataProviderFromHttpRouteData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+
+namespace HigLabo.Web.Mvc
+{
+    public class ParameterDataProviderFromHttpRouteData : ParameterDataProvider
+    {
+        public override string Name
+        {
+            get { return "ParameterDataProviderFromHttpRouteData"; }
+        }
+        public override Dictionary<string, string> GetDataSource(HttpActionContext context)
+        {
+            var d = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase);
+            foreach (var item in context.ControllerContext.RouteData.Values)
+            {
+                d[item.Key] = item.Value.ToString();
+            }
+            return d;
+        }
+    }
+}
