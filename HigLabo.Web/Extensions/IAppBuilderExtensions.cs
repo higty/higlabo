8810a33d34f2957dce5ba187de72718cded58dc3
higty <higuchihiguchihiguchi@hotmail.com>
2016-02-25 11:31:51 +0900

    Add IAppBuilderExtensions class.

---
+++ HigLabo.Web/Extensions/IAppBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HigLabo.Web
+{
+    /// <summary>
+    /// Reference: http://www.zpqrtbnk.net/posts/appdomains-threads-cultureinfos-and-paracetamol
+    /// </summary>
+    public static class IAppBuilderExtensions
+    {
+        public static void SanitizeThreadCulture(this IAppBuilder app)
+        {
+            var currentCulture = CultureInfo.CurrentCulture;
+
+            // at the top of any culture should be the invariant culture,
+            // find it doing an .Equals comparison ensure that we will
+            // find it and not loop endlessly
+            var invariantCulture = currentCulture;
+            while (invariantCulture.Equals(CultureInfo.InvariantCulture) == false)
+                invariantCulture = invariantCulture.Parent;
+
+            if (ReferenceEquals(invariantCulture, CultureInfo.InvariantCulture))
+                return;
+
+            var thread = Thread.CurrentThread;
+            thread.CurrentCulture = CultureInfo.GetCultureInfo(thread.CurrentCulture.Name);
+            thread.CurrentUICulture = CultureInfo.GetCultureInfo(thread.CurrentUICulture.Name);
+        }
+    }
+}
