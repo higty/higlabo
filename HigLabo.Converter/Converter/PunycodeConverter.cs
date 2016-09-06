a3d9cc07f21e3831e8573568383f521f4ec5fbeb
higty <higuchihiguchihiguchi@hotmail.com>
2016-09-06 17:14:11 +0900

    Add PunycodeConverter.

---
+++ HigLabo.Converter/Converter/PunycodeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace HigLabo.Core
+{
+    public class PunycodeConverter
+    {
+        public String Encode(String url)
+        {
+            var u = new Uri(url);
+            var idn = new IdnMapping();
+            var hostName = idn.GetAscii(u.Host);
+            return url.Replace(u.Host, hostName);
+        }
+        public String Decode(String url)
+        {
+            var u = new Uri(url);
+            var idn = new IdnMapping();
+            var hostName = idn.GetUnicode(u.Host);
+            return url.Replace(u.Host, hostName);
+        }
+    }
+}
