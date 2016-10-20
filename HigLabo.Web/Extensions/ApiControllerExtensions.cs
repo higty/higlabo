317b0a920140bbab4236367507215724c26d018e
higty <higuchihiguchihiguchi@hotmail.com>
2016-10-20 15:46:25 +0900

    Add ApiControllerExtensions class.

---
+++ HigLabo.Web/Extensions/ApiControllerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+namespace HigLabo.Web.Mvc
+{
+    public static class ApiControllerExtensions
+    {
+        public static ApiJsonReslut<T> ApiJsonReslut<T>(this ApiController controller, HttpStatusCode statusCode, T data)
+        {
+            return new ApiJsonReslut<T>(controller.Request, statusCode, data);
+        }
+    }
+}
