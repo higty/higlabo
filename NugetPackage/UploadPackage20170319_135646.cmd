6bdd13879f75644ad59ba9aa996b5feb28837758
higty <higuchihiguchihiguchi@hotmail.com>
2017-03-19 14:26:19 +0900

    Remove Nuget dependency and upload new version to nuget.

---
+++ NugetPackage/UploadPackage20170319_135646.cmd
@@ -0,0 +1,4 @@
+nuget push HigLabo.Net._Net_3_5.1.0.2.2.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push HigLabo.Net._Net_4_0.1.0.2.2.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push HigLabo.Net.1.0.2.2.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+pause
