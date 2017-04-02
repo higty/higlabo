e9eca988313f10ea99437511b8b387a470632455
higty <higuchihiguchihiguchi@hotmail.com>
2017-04-02 20:22:44 +0900

    Modify DbSharp Nuget package.

---
+++ NugetPackage/UploadPackage20170326_220217.cmd
@@ -0,0 +1,7 @@
+nuget push HigLabo.DbSharp.CodeGenerator.1.2.2.1.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push HigLabo.DbSharp.Redis.1.1.0.1.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push HigLabo.DbSharp.SchemaData.1.1.0.2.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push HigLabo.DbSharp.Service.1.2.1.2.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push HigLabo.DbSharp.SqlServer.1.1.0.2.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push HigLabo.DbSharp.1.2.0.3.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+pause
