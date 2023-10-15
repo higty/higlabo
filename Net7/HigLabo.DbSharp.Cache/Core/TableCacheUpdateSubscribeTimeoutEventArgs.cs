b073caac80ca8e0d6767505a527666e4979d0859
higty <higuchihiguchihiguchi@hotmail.com>
2023-10-15 16:38:50 +0900

    Fix table cache.

---
+++ Net7/HigLabo.DbSharp.Cache/Core/TableCacheUpdateSubscribeTimeoutEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigLabo.DbSharp
+{
+	public class TableCacheUpdateSubscribeTimeoutEventArgs : EventArgs
+	{
+		public String TableName { get; set; }
+
+		public TableCacheUpdateSubscribeTimeoutEventArgs(String tableName)
+		{
+			this.TableName = tableName;
+		}
+	}
+}
