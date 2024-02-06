1f220254a04cdc6f89976cd0b53d2be82ccddfc8
higty <higuchihiguchihiguchi@hotmail.com>
2024-02-07 00:38:25 +0900

    Add PagingPanel to HigLabo.Web.RazorComponent. Move image files.

--- Net8/HigLabo.Web.RazorComponent/Core/UrlInfo.cs
+++ Net8/HigLabo.Web.RazorComponent/Core/UrlInfo.cs
@@ -4,7 +4,7 @@
     {
         public static string ImageWebSite { get; set; } = "https://localhost:7014/img";
 
-        public static string GetIconUrl(Higlabo__ fileName)
+        public static string GetIconUrl(Higlabo__icon fileName)
         {
             return $"{ImageWebSite}/{fileName.GetBlobName()}";
         }
