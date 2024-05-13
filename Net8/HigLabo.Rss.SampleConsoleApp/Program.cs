f9ac3f558a44a8f50c149aed8e000e615311e156
higty <higuchihiguchihiguchi@hotmail.com>
2024-05-13 23:11:38 +0900

    Fix sample code of HigLabo.Rss.SampleConsoleApp.

--- Net8/HigLabo.Rss.SampleConsoleApp/Program.cs
+++ Net8/HigLabo.Rss.SampleConsoleApp/Program.cs
@@ -5,9 +5,10 @@ var xml = await File.ReadAllTextAsync(Path.Combine(Environment.CurrentDirectory,
 var parser = new Rss_2_0_Parser();
 var feed = parser.Parse(xml);
 
-foreach (var item in feed.Items)
+foreach (RssItem_2_0 item in feed.Items)
 {
     Console.WriteLine(item.ToString());
+    Console.WriteLine(item.Author);
     Console.WriteLine("--------------");
 }
 Console.WriteLine("Parse success");
