8c87c33ffdd00cfaf8fe1841f03602d1af08f836
higty <higuchihiguchihiguchi@hotmail.com>
2024-09-27 15:42:04 +0900

    Add HigLabo.Web.Htmx project.

---
+++ Net8/HigLabo.Web.Htmx.SampleWebSite/Entity/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace HigLabo.Web.Htmx.SampleWebSite.Entity
+{
+    public class Department
+    {
+        public string Name { get; set; } = "";
+        public List<Department>? Departments { get; set; } 
+    }
+}
---
+++ Net8/HigLabo.Web.Htmx.SampleWebSite/Entity/HtmxPostData.cs
@@ -0,0 +1,21 @@
+﻿namespace HigLabo.Web.Htmx.SampleWebSite.Entity
+{
+    public class HtmxPostData
+    {
+        public List<string> Permissions { get; set; } = new();
+        public List<PermissionSettings> PermissionObjects { get; set; } = new();
+        public List<Person> Persons { get; set; } = new();
+    }
+    public class PermissionSettings
+    {
+        public string Name { get; set; } = "";
+        public string Value { get; set; } = "";
+        public bool Checked { get; set; }
+    }
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; } 
+        public bool IsAdmin { get; set; }
+    }
+}
